9185e82ef2e88d031062cd296bc6bd22af25cb85
tardis03 <111162303+argf013@users.noreply.github.com>
2023-02-09 19:28:41 +0700

    fixing e2e testing

--- e2e/Login.spec.js
+++ e2e/Login.spec.js
@@ -1,67 +1,50 @@
+/* eslint-disable testing-library/await-async-utils */
 /* eslint-disable no-undef */
 
+// Scenario for logging in and signing out
 Feature('Login');
-
 Scenario('Logging in and Sign Out', ({ I }) => {
-    const id = 'example@yahoo.com'
+    const id = 'example@yahoo.com';
     I.amOnPage('');
-    I.seeElement('.email')
-    I.seeElement('.password')
+    I.seeElement('.email');
+    I.seeElement('.password');
     I.fillField({ css: 'input[type="email"]' }, id);
     I.fillField({ css: 'input[type="password"]' }, '123456');
     I.click({ css: 'button[type="submit"]' });
-    I.wait(3).then(() => {
-        console.log('3 seconds have passed');
-      });
+    I.wait(3);
     I.see('Welcome');
-    I.click('OK'); // Klik OK pada SweetAlert
-    I.wait(2).then(() => {
-        console.log('3 seconds have passed');
-      });
-    I.see(id)
-    I.click('Sign Out');
-    I.wait(2).then(() => {
-        console.log('3 seconds have passed');
-      });
     I.click('OK');
+    I.wait(2);
+    I.see(id);
+
+    I.click('Sign Out');
+
+    I.wait(2);
+
+    I.click('OK');
+
     I.see('Login Page');
 });
 
 
-function randomString(length) {
-    let result = '';
-    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let charactersLength = characters.length;
-    for (let i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-  }
-  
-  let random_string = randomString(10);
+// create random email and password
+const email = `${Math.random().toString(36).substring(7)}@example.com`;
+const password = Math.random().toString(36).substring(7);
 
 Scenario('Register', ({ I }) => {
     I.amOnPage('regist');
-    I.wait(2).then(() => {
-        console.log('3 seconds have passed');
-      });
+    I.wait(2)
     I.see('Register Page');
-    I.fillField({ css: 'input[type="email"]' }, `${random_string}@yahoo.com`);
-    I.fillField({ css: 'input[type="password"]' }, random_string);
+    I.fillField({ css: 'input[type="email"]' }, email);
+    I.fillField({ css: 'input[type="password"]' }, password);
     I.click({ css: 'button[type="submit"]' });
-    I.wait(3).then(() => {
-        console.log('3 seconds have passed');
-      });
+    I.wait(3)
     I.see('Register Success');
     I.click('OK');
-    I.wait(3).then(() => {
-        console.log('3 seconds have passed');
-      });
+    I.wait(3)
     I.see('Sign Out');
     I.click('Sign Out');
-    I.wait(2).then(() => {
-        console.log('3 seconds have passed');
-      });
+    I.wait(2)
     I.click('OK');
-    I.see('Login Page')
-})
+    I.see('Login Page');
+});
