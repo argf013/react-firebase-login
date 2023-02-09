b97c9c3a44feb0ab480c1e322d8bf6a96560ab41
tardis03 <111162303+argf013@users.noreply.github.com>
2023-02-09 19:05:31 +0700

    fix

--- e2e/Login.spec.js
+++ e2e/Login.spec.js
@@ -28,40 +28,40 @@ Scenario('Logging in and Sign Out', ({ I }) => {
 });
 
 
-// function randomString(length) {
-//     let result = '';
-//     let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-//     let charactersLength = characters.length;
-//     for (let i = 0; i < length; i++) {
-//       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-//     }
-//     return result;
-//   }
+function randomString(length) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
   
-//   let random_string = randomString(10);
+  let random_string = randomString(10);
 
-// Scenario('Register', ({ I }) => {
-//     I.amOnPage('regist');
-//     I.wait(2).then(() => {
-//         console.log('3 seconds have passed');
-//       });
-//     I.see('Register Page');
-//     I.fillField({ css: 'input[type="email"]' }, `${random_string}@yahoo.com`);
-//     I.fillField({ css: 'input[type="password"]' }, random_string);
-//     I.click({ css: 'button[type="submit"]' });
-//     I.wait(3).then(() => {
-//         console.log('3 seconds have passed');
-//       });
-//     I.see('Register Success');
-//     I.click('OK');
-//     I.wait(3).then(() => {
-//         console.log('3 seconds have passed');
-//       });
-//     I.see('Sign Out');
-//     I.click('Sign Out');
-//     I.wait(2).then(() => {
-//         console.log('3 seconds have passed');
-//       });
-//     I.click('OK');
-//     I.see('Login Page')
-// })
+Scenario('Register', ({ I }) => {
+    I.amOnPage('regist');
+    I.wait(2).then(() => {
+        console.log('3 seconds have passed');
+      });
+    I.see('Register Page');
+    I.fillField({ css: 'input[type="email"]' }, `${random_string}@yahoo.com`);
+    I.fillField({ css: 'input[type="password"]' }, random_string);
+    I.click({ css: 'button[type="submit"]' });
+    I.wait(3).then(() => {
+        console.log('3 seconds have passed');
+      });
+    I.see('Register Success');
+    I.click('OK');
+    I.wait(3).then(() => {
+        console.log('3 seconds have passed');
+      });
+    I.see('Sign Out');
+    I.click('Sign Out');
+    I.wait(2).then(() => {
+        console.log('3 seconds have passed');
+      });
+    I.click('OK');
+    I.see('Login Page')
+})
